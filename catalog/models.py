d59ad784cb30621a18fa3bf71053e5b7e56ed0b4
dan.toropov <dan.toropov@gmail.com>
2024-08-01 22:00:48 +0300

    begin hometask 6_3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    descriptions = models.TextField(blank=True)
+    ordering = ['name']
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(to=Category, on_delete=models.PROTECT, related_name='products')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+    def __str__(self):
+        return f"{self.name}"
