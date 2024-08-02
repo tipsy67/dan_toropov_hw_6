9b1d37c614a70ea27cd665bdc0837dbd7997c2ba
dan.toropov <dan.toropov@gmail.com>
2024-08-02 22:55:17 +0300

    create data

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,7 +6,8 @@ from .models import Product, Category
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
-
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    descriptions = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     ordering = ['name']
 
     class Meta:
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
