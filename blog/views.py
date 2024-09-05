8bdcd4bb22a010be8537b055c4937af3e6c1ed22
dan.toropov <dan.toropov@gmail.com>
2024-09-05 22:39:25 +0300

    create newsletter utils .

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-
-from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 
